53f828d366b20229387eac51fb3ed82ee0ca0e83
vbhambhu <v.bhambhu083@gmail.com>
2017-09-12 23:02:35 +0100

    menu lang setup

--- src/main/java/com/gochyou/MenuAdvice.java
+++ src/main/java/com/gochyou/MenuAdvice.java
@@ -1,26 +1,36 @@
 package com.gochyou;
 
 import com.gochyou.models.KeyValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 @ControllerAdvice
 public class MenuAdvice {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @ModelAttribute
-    public void addAttributes(Model model) {
+    public void addAttributes(Locale locale, Model model) {
+
+        String home = messageSource.getMessage("menu.home", new Object[]{}, locale);
+        String howitwork = messageSource.getMessage("menu.howitwork", new Object[]{}, locale);
+        String whoweare = messageSource.getMessage("menu.whoweare", new Object[]{}, locale);
+        String contact = messageSource.getMessage("menu.contact", new Object[]{}, locale);
+
+
+
 
         List<KeyValue> menu = new ArrayList<KeyValue>();
-        menu.add(new KeyValue("/", "Home"));
-        menu.add(new KeyValue("/how-it-works", "How it works?"));
-        menu.add(new KeyValue("/who-we-are", "Who we are?"));
-        menu.add(new KeyValue("/contact", "Contact"));
+        menu.add(new KeyValue("/", home));
+        menu.add(new KeyValue("/how-it-works", howitwork));
+        menu.add(new KeyValue("/who-we-are", whoweare));
+        menu.add(new KeyValue("/contact", contact));
 
 
 
